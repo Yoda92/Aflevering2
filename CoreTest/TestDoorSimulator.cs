698c39e154f1ca3c817daf3a5e6b2a731f5abb46
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-03-11 14:56:40 +0100

    Finished door testing

--- CoreTest/TestDoorSimulator.cs
+++ CoreTest/TestDoorSimulator.cs
@@ -5,17 +5,48 @@ namespace CoreTest
 {
     public class TestDoorSimulator
     {
-        private DoorSimulator door;
+        private DoorSimulator _door;
+        private DoorStateEventArgs _receivedEventArgs; 
         [SetUp]
         public void Setup()
         {
-           door = new DoorSimulator(); 
+            _receivedEventArgs = null;
+           _door = new DoorSimulator();
+           _door.DoorStateChangedEvent += (o, args) => { _receivedEventArgs = args; };
         }
 
         [Test]
         public void ctor_LockedIsFalse()
         {
-            Assert.That(door.IsLocked, Is.False);
+            Assert.That(_door.IsLocked, Is.False);
+        }
+
+        [Test]
+        public void LockingSetsStateToLocked()
+        {
+            _door.LockDoor();
+            Assert.That(_door.IsLocked, Is.True);
+        }
+
+        [Test]
+        public void UnlockingSetsStateToUnlocked()
+        {
+           _door.UnlockDoor();
+           Assert.That(_door.IsLocked, Is.False);
+        }
+
+        [Test]
+        public void OpeningDoorInvokesDoorStateEvent()
+        {
+            _door.SimulateDoorOpen();
+            Assert.That(_receivedEventArgs.Open, Is.True);
+        }
+
+        [Test]
+        public void ClosingDoorInvokesDoorStateEvent()
+        {
+            _door.SimulateDoorClose();
+            Assert.That(_receivedEventArgs.Open, Is.False);
         }
     }
 }
