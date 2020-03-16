a42be7311c1c7389ee9762a04c8a980c1a38e997
Bendix <bbendixx@gmail.com>
2020-03-16 11:05:30 +0100

    Logfile test in progress

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,11 +8,6 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-            Core.LogFile L = new Core.LogFile();
-
-            L.LogDoorLocked(16);
-            L.LogDoorUnlocked(20);
         }
     }
 }
