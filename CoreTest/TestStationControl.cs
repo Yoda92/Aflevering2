2a42a3fea0d5b825aad3b104a29a13f727245e3c
Yoda92 <andersfisker1007@outlook.com>
2020-03-16 12:05:14 +0100

    Fixed typo in station control test-case

--- CoreTest/TestStationControl.cs
+++ CoreTest/TestStationControl.cs
@@ -28,8 +28,8 @@ namespace CoreTest
         
         [TestCase(false, StationControl.LadeskabState.DoorOpen, "Dør er lukket. Indlæs RFID.", StationControl.LadeskabState.Available)]
         [TestCase(true, StationControl.LadeskabState.DoorOpen, "Dør er åben", StationControl.LadeskabState.DoorOpen)]
-        [TestCase(false, StationControl.LadeskabState.Available, "Dør er åben. Tilslut telefon.", StationControl.LadeskabState.DoorOpen)]
-        [TestCase(true, StationControl.LadeskabState.Available, "Dør er lukket. Indlæs RFID", StationControl.LadeskabState.Available)]
+        [TestCase(true, StationControl.LadeskabState.Available, "Dør er åben. Tilslut telefon.", StationControl.LadeskabState.DoorOpen)]
+        [TestCase(false, StationControl.LadeskabState.Available, "Dør er lukket. Indlæs RFID", StationControl.LadeskabState.Available)]
         public void HandleDoorStateChangedTest(bool open, StationControl.LadeskabState inputState, string outputString, StationControl.LadeskabState outputState)
         {
             _sc._state = inputState;
